38782dee18de4f58172c43ef043c9ce6a219402d
age <hiroshi.agehara@goodpatch.com>
2022-06-29 01:44:47 +0900

    👍 reply when master is agehara

--- helloworld/src/Lib.hs
+++ helloworld/src/Lib.hs
@@ -2,8 +2,11 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Monad
+
 someFunc :: IO ()
 someFunc = do 
   putStrLn "welcome"
   name <- getLine
-  putStrLn $ "hello," ++ name
+  when (name == "agehara") $ do
+    putStrLn $ "hello, master " ++ name
